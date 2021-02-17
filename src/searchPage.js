c6b9f00773ebe9d642dd6fbaec5aff4fa96a17bf
patrick hrabos <phrabos@gmail.com>
2021-02-16 19:52:24 -0500

    server side sorting setup

--- src/searchPage.js
+++ src/searchPage.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react'
 import PokeList from './pokeList.js';
-// import data from './data.js';
 import './App.css';
 import SideBar from './sideBar.js';
 import request from 'superagent';
 import Spinner from './spinner.js'
-import { nativeTouchData } from 'react-dom/test-utils';
 
 export default class SearchPage extends Component {
     state = {
         objects: [],
-        filteredObjects: [],
         aToZ: '',
         category: '',
         search: '',
-        egg: '',
+        shape: '',
         loading: false,
     }
     componentDidMount = async () =>{
-        await this.loadPokedex();
+        await this.fetchPokedex();
 
     }
-    loadPokedex = async () => {
-      this.setState({
+
+    fetchPokedex = async () => {
+        this.setState({
         loading: true,
       })
-      const data = await request.get ('https://pokedex-alchemy.herokuapp.com/api/pokedex?page=1&perPage=50');
+      const data = await request.get (`https://pokedex-alchemy.herokuapp.com/api/pokedex?page=1&perPage=50&pokemon=${this.state.search}&shape=${this.state.shape}&sort=${this.state.category}&direction=${this.state.aToZ}`);
       this.setState ({
         objects: data.body.results,
         loading: false
@@ -35,75 +33,70 @@ export default class SearchPage extends Component {
     handleSearchChange = async (e) => {
         this.setState({
           search: e.target.value,
-          loading: true,
         })
-        const query = this.state.search
-        const data = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${query}`)
-        this.setState({objects: data.body.results, loading: false})
       }
 
     handleSearchButton = async () => {
-      const query = this.state.search
-      const data = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${query}`)
-      this.setState({objects: data.body.results})
+      await this.fetchPokedex()
     }
-    handleAZChange = (e) => {
-        this.setState({
+    handleAZChange = async (e) => {
+        await this.setState({
           aToZ: e.target.value,
         })
+        await this.fetchPokedex();
       }
-    handleSortChange = (e) => {
-      this.setState({
+    handleSortChange = async (e) => {
+      await this.setState({
         category: e.target.value,
-        
       })
+      await this.fetchPokedex();
     }
-    handleClearButtonChange = (e) => {
-      this.loadPokedex()
-      this.setState({
+    handleClearButtonChange = async () => {
+      await this.setState({
         aToZ: '',
         category: '',
         search: '',
-          egg: '',
-        })
-      }
-      handleEggChange = (e) => {
-      this.setState({
-        egg: e.target.value,
+        shape: '',
       })
+      await this.fetchPokedex()
+      }
+      handleShapeChange = async (e) => {
+      await this.setState({
+        shape: e.target.value,
+      })
+      await this.fetchPokedex();
     }   
     
     render() {
-        console.log(this.state.objects)
-        console.log(this.state.category)
-        const sortedData = this.state.objects.sort((a,b) =>{
 
-          if(this.state.category === '')return this.state.objects;
-          if(this.state.category && this.state.aToZ !== 'Descending')return  a[this.state.category].localeCompare(b[this.state.category]);
-          if(this.state.category && this.state.aToZ === 'Descending') return b[this.state.category].localeCompare(a[this.state.category]);
-          else return true;
-        } 
-        )
+        // const sortedData = this.state.objects.sort((a,b) =>{
+
+        //   if(this.state.category === '')return this.state.objects;
+        //   if(this.state.category && this.state.aToZ !== 'Descending')return  a[this.state.category].localeCompare(b[this.state.category]);
+        //   if(this.state.category && this.state.aToZ === 'Descending') return b[this.state.category].localeCompare(a[this.state.category]);
+        //   else return true;
+        // } 
+        // )
         
-        const filteredDataObjects = sortedData.filter((object) => {
-          if (this.state.search === '' && this.state.egg ==='') return this.state.objects;
-          if (!this.state.search && this.state.egg){
-            if (this.state.egg === object.egg_group_2) return true;
-          }
-          if (this.state.search && !this.state.egg){
-            if (object.pokemon.includes(this.state.search)) return true;
-          }
-          if (this.state.search && this.state.egg){
-            if (this.state.egg === object.egg_group_2 && object.pokemon.includes(this.state.search)) return true;
-          }
+        // const filteredDataObjects = sortedData.filter((object) => {
+        //   if (this.state.search === '' && this.state.shape ==='') return this.state.objects;
+        //   if (!this.state.search && this.state.shape){
+        //     if (this.state.shape === object.shape_group_2) return true;
+        //   }
+        //   if (this.state.search && !this.state.shape){
+        //     if (object.pokemon.includes(this.state.search)) return true;
+        //   }
+        //   if (this.state.search && this.state.shape){
+        //     if (this.state.shape === object.shape_group_2 && object.pokemon.includes(this.state.search)) return true;
+        //   }
 
-          return false;
-        }
-        )
+        //   return false;
+        // }
+        // )
    
         return (
             <>
-            {this.state.loading && <Spinner />}
+           
             <SideBar 
             searchValue = {this.handleSearchChange}
             handleSearchButton = {this.handleSearchButton}
@@ -113,17 +106,17 @@ export default class SearchPage extends Component {
             currentValue={this.state.category}
             placeholder1={'-Category-'}
             handleSortChange2 = {this.handleAZChange}
-            options2 = {['Ascending', 'Descending']}
+            options2 = {['asc', 'desc']}
             currentValue2={this.state.aToZ}
             placeholder2={'-Sort Order-'}
-            handleEggChange={this.handleEggChange}
-
+            handleShapeChange={this.handleShapeChange}
             />
-            <div className='ul-div'>
-                <PokeList 
-                dataObjects={filteredDataObjects}
-                />
-            </div>
+            {this.state.loading ? <Spinner />
+            : <div className='ul-div'>
+            <PokeList 
+            dataObjects={this.state.objects}
+            />
+            </div>}
             </>
 
         )
